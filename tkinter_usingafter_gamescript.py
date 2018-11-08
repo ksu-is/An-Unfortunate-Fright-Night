3e0c6e34058a0f933fcd8ad87b6d8662a96fcc61
shannonjones4 <44588908+shannonjones4@users.noreply.github.com>
2018-11-08 16:47:32 -0500

    Update tkinter_usingafter_gamescript.py

--- tkinter_usingafter_gamescript.py
+++ tkinter_usingafter_gamescript.py
@@ -17,14 +17,15 @@ E1 = Entry(root, bd =5)
 E1.pack(side=LEFT)
 
 tiles_letter = ['a', 'b', 'c', 'd', 'e']
+tiles_letter.reverse()
 
 def add_letter():
-    rand = random.choice(tiles_letter)
-    tile_frame = Label(frame, text=rand)
+    tile_frame = Label(frame, text=tiles_letter.pop())
     tile_frame.pack()
     root.after(500, add_letter)
-    tiles_letter.remove(rand)  # remove that tile from list of tiles
 
-
+    if tiles_letter == []:
+        return tiles_letter.pop()
+    
 root.after(0, add_letter)  # add_letter will run as soon as the mainloop starts.
 root.mainloop()
