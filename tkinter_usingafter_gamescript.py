fa0ba8e7a1dad019d0d197bb5360a234574252f1
shannonjones4 <44588908+shannonjones4@users.noreply.github.com>
2018-11-29 16:21:02 -0500

    Update tkinter_usingafter_gamescript.py

--- tkinter_usingafter_gamescript.py
+++ tkinter_usingafter_gamescript.py
@@ -3,7 +3,6 @@ import time
 from tkinter import *
 
 
-
 root = Tk()
 
 #title of game
@@ -14,40 +13,26 @@ w.pack()
 frame = Frame(root, width=600, height=600)
 frame.pack()
 
-#intro text, doesn't work
-
-intro1 = Label(text="It was a dark and spooky night.")
-intro1.grid(column=0, row=1)
-intro2 = tk.Label(text="You come across a haunted, abandoned house on your way to your friend's Halloween party.")
-intro2.grid(column=0, row=2)
-intro3 = tk.Label(text="You are curious to see what is inside.")
-intro3.grid(column=0, row=3)
-intro4 = tk.Label(text='Go inside or Go to the party?')
-intro4.grid(column=0, row=4)
-intro5 = tk.Label(text='...')
-intro5.grid(column=0, row=5)
-
 #text box choose your option based on mutiple choice answers
-
 L1 = Label(root, text="Choose your option: ")
 L1.pack(side=LEFT)
 E1 = Entry(root, bd =5)
 E1.pack(side=LEFT)
 
+#intro text
+tiles_intro = ['It was a dark and spooky night.', 'You come across a haunted, abandoned house on your way to a Halloween party.', 'You are curious to see what is inside.', 'A. Go inside', 'B. Go to the party?']
+tiles_intro.reverse()
 
 
-#mutiple choice options appear in gui
-tiles_letter = ['a', 'b', 'c', 'd', 'e']
-tiles_letter.reverse()
+def add_intro():
+    if len(tiles_intro) > 0:
+        intro_frame = Label(frame, text=tiles_intro.pop())
+        intro_frame.pack()
+        root.after(500, add_intro)
+        
 
 
-def add_letter():
-    if len(tiles_letter) > 0:
-        tile_frame = Label(frame, text=tiles_letter.pop())
-        tile_frame.pack()
-        root.after(500, add_letter)
-
-
-
-root.after(0, add_letter)  # add_letter will run as soon as the mainloop starts.
+root.after(0, add_intro)  # add_intro will run as soon as the mainloop starts.
 root.mainloop()
+
+
